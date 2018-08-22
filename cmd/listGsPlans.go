e3f1faadc991d091d1abc0b51d81fb23f6862d5b
Ryuichi Hoshi <hoshi.ryuichi@gmail.com>
2018-08-22 11:34:45 +0900

    Added ListPlans service for Ground Station. (#8)
    * Added ListPlans service for Ground Station.
    
    * Small fixes.
    
    * Fixed typo.
    
    * Added duration flag.
    
    * Added notes saying time need to be UTC.
    
    * Removed redundant messages.
    
    * Added default value in flag help texts.
    
    * Removed util/timeUtil. Fixed texts.
    
    * Fixed typo.

---
+++ cmd/listGsPlans.go
@@ -0,0 +1,84 @@
+// Copyright © 2018 Infostellar, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/infostellarinc/stellarcli/pkg/plan"
+)
+
+const (
+	// Time format used to parse "after" and "before" flags.
+	timeFormat = "2006-01-02 15:04:05"
+	// Default time range used when end time is not specified.
+	defaultDurationInDays = 31
+	// Maximum value of duration in days.
+	maxDurationInDays = 31
+)
+
+var (
+	flgAOSAfter  string
+	flgAOSBefore string
+	flgDuration  uint8
+)
+
+// listGSPlansCmd represents the ground station command
+var listGSPlansCmd = &cobra.Command{
+	Use:   "list-plans [Ground Station ID]",
+	Short: "Lists plans on a ground station.",
+	Long: `Lists plans on a ground station. Plans having AOS between the given time range are returned. 
+When run with default flags, plans in the next 31 days are returned.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("accepts 1 arg(s), received %d", len(args))
+		}
+
+		if flgDuration == 0 || flgDuration > maxDurationInDays {
+			return fmt.Errorf("Invalid value of duration: %v. Expected value: 1-%v", flgDuration, maxDurationInDays)
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+
+		aosAfter, err := time.Parse(timeFormat, flgAOSAfter)
+		if err != nil {
+			aosAfter = time.Now()
+		}
+
+		aosBefore, err := time.Parse(timeFormat, flgAOSBefore)
+		if err != nil {
+			aosBefore = aosAfter.AddDate(0, 0, int(flgDuration))
+		}
+
+		plan.ListPlans(args[0], aosAfter, aosBefore)
+	},
+}
+
+func init() {
+	gsCmd.AddCommand(listGSPlansCmd)
+
+	listGSPlansCmd.Flags().StringVarP(&flgAOSAfter, "aos-after", "a", "",
+		`The start time (UTC) of the range of plans to list (inclusive). Example: "2006-01-02 15:04:00 (default current time"`)
+	listGSPlansCmd.Flags().StringVarP(&flgAOSBefore, "aos-before", "b", "",
+		`The end time (UTC) of the range of plans to list (exclusive). Example: "2006-01-02 15:14:00" `+
+			fmt.Sprintf("(default aos-after + %d days", defaultDurationInDays))
+	listGSPlansCmd.Flags().Uint8VarP(&flgDuration, "duration", "d", defaultDurationInDays,
+		fmt.Sprintf("Duration of the range of plans to list (1-%v), in days. Duration will be ignored when aos-before is specified.", maxDurationInDays))
+}
---
+++ pkg/plan/plan.go
@@ -0,0 +1,108 @@
+// Copyright © 2018 Infostellar, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plan
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+
+	"github.com/infostellarinc/go-stellarstation/api/v1/groundstation"
+	"github.com/infostellarinc/stellarcli/pkg/apiclient"
+)
+
+// Default format of time.Timestamp when converting it to a textual representation
+const defaultFormat = time.RFC3339
+
+// Default separator between columns
+const defaultSeparator = ","
+
+// Headers of columns
+var headers = []string{
+	"PLAN_ID",
+	"AOS_TIME",
+	"LOS_TIME",
+	"DOWNLINK_CENTER_FREQUENCY_HZ",
+	"UPLINK_CENTER_FREQUENCY_HZ",
+	"TLE_LINE__1",
+	"TLE_LINE__2",
+}
+
+// ListPlans returns a list of plans for a given ground staion
+func ListPlans(id string, aosAfter, aosBefore time.Time) {
+
+	conn, err := apiclient.Dial()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	aosAfterTimestamp, err := ptypes.TimestampProto(aosAfter.UTC())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	aosBeforeTimestamp, err := ptypes.TimestampProto(aosBefore.UTC())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := groundstation.NewGroundStationServiceClient(conn)
+	request := &groundstation.ListPlansRequest{
+		GroundStationId: id,
+		AosAfter:        aosAfterTimestamp,
+		AosBefore:       aosBeforeTimestamp,
+	}
+
+	result, err := client.ListPlans(context.Background(), request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO(hoshir): Accepts time format from commandline options
+	layout := defaultFormat
+
+	// TODO(hoshir): Accepts a separator from commandline options
+	sep := defaultSeparator
+
+	fmt.Println(strings.Join(headers, sep))
+	for _, plan := range result.Plan {
+		aos, err := ptypes.Timestamp(plan.AosTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		los, err := ptypes.Timestamp(plan.LosTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var b bytes.Buffer
+		b.WriteString(fmt.Sprintf("%q%s", plan.PlanId, sep))
+		b.WriteString(fmt.Sprintf("%q%s", aos.Format(layout), sep))
+		b.WriteString(fmt.Sprintf("%q%s", los.Format(layout), sep))
+		b.WriteString(fmt.Sprintf("%v%s", plan.DownlinkCenterFrequencyHz, sep))
+		b.WriteString(fmt.Sprintf("%v%s", plan.UplinkCenterFrequencyHz, sep))
+		b.WriteString(fmt.Sprintf("%q%s", plan.Tle.Line_1, sep))
+		b.WriteString(fmt.Sprintf("%q%s", plan.Tle.Line_2, sep))
+
+		fmt.Println(b.String())
+	}
+}
