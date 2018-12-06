f58e37df9d56ce25d8ccdae6ee40b6514e811fa4
Ryuichi Hoshi <hoshi.ryuichi@gmail.com>
2018-12-06 12:09:51 +0900

    Added addTle and getTle command. (#31)

---
+++ pkg/satellite/tle/add_tle.go
@@ -0,0 +1,61 @@
+// Copyright © 2018 Infostellar, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tle
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	stellarstation "github.com/infostellarinc/go-stellarstation/api/v1"
+	"github.com/infostellarinc/go-stellarstation/api/v1/orbit"
+	"github.com/infostellarinc/stellarcli/pkg/apiclient"
+	"github.com/infostellarinc/stellarcli/util/printer"
+)
+
+type AddTLEOptions struct {
+	Printer     printer.Printer
+	SatelliteID string
+	Line1       string
+	Line2       string
+}
+
+// AddTLE adds a new TLE to a given satellite.
+func AddTLE(o *AddTLEOptions) {
+	conn, err := apiclient.Dial()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := stellarstation.NewStellarStationServiceClient(conn)
+	tle := &orbit.Tle{
+		Line_1: o.Line1,
+		Line_2: o.Line2,
+	}
+	request := &stellarstation.AddTleRequest{
+		SatelliteId: o.SatelliteID,
+		Tle:         tle,
+	}
+
+	_, err = client.AddTle(context.Background(), request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer o.Printer.Flush()
+	message := fmt.Sprintf("Succeeded to add the TLE.")
+	o.Printer.Write([]interface{}{message})
+}
---
+++ pkg/satellite/tle/get_tle.go
@@ -0,0 +1,61 @@
+// Copyright © 2018 Infostellar, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tle
+
+import (
+	"context"
+	"log"
+
+	stellarstation "github.com/infostellarinc/go-stellarstation/api/v1"
+	"github.com/infostellarinc/stellarcli/pkg/apiclient"
+	"github.com/infostellarinc/stellarcli/util/printer"
+)
+
+// Headers of columns
+var headers = []interface{}{
+	"TLE_LINE_1",
+	"TLE_LINE_2",
+}
+
+type GetTLEOptions struct {
+	Printer     printer.Printer
+	SatelliteId string
+}
+
+// GetTLE returns a TLE for the given satellite.
+func GetTLE(o *GetTLEOptions) {
+	conn, err := apiclient.Dial()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := stellarstation.NewStellarStationServiceClient(conn)
+	request := &stellarstation.GetTleRequest{SatelliteId: o.SatelliteId}
+
+	result, err := client.GetTle(context.Background(), request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer o.Printer.Flush()
+	o.Printer.Write(headers)
+
+	record := []interface{}{
+		result.Tle.Line_1,
+		result.Tle.Line_2,
+	}
+	o.Printer.Write(record)
+}
