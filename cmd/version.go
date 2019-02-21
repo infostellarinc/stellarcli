ac07dea7e7f20340d2ba2a04db7ad1036abda770
Ryuichi Hoshi <ryuichi@istellar.jp>
2019-02-21 12:06:47 +0900

    Added version command.

---
+++ cmd/version.go
@@ -0,0 +1,42 @@
+// Copyright © 2019 Infostellar, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "v0.1.1"
+)
+
+// Create version command.
+func NewVersionCommand() *cobra.Command {
+
+	command := &cobra.Command{
+		Use:   "version",
+		Short: "Version of stellar",
+		Long:  "Version prints the stellar version.",
+
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version)
+		},
+	}
+
+	return command
+
+}
