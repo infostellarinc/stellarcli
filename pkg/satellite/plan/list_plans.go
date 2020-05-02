1bee03b8afd2014777ed1c303226c2de17ff3484
David Kuo <david.j.kuo@gmail.com>
2020-05-02 22:35:31 +0900

    nit

--- pkg/satellite/plan/list_plans.go
+++ pkg/satellite/plan/list_plans.go
@@ -23,7 +23,6 @@ import (
 	stellarstation "github.com/infostellarinc/go-stellarstation/api/v1"
 	"github.com/infostellarinc/stellarcli/pkg/apiclient"
 	log "github.com/infostellarinc/stellarcli/pkg/logger"
-
 	"github.com/infostellarinc/stellarcli/util/printer"
 )
 
--- pkg/satellite/tle/set_tle_source.go
+++ pkg/satellite/tle/set_tle_source.go
@@ -19,10 +19,9 @@ import (
 	"fmt"
 	"strings"
 
-	log "github.com/infostellarinc/stellarcli/pkg/logger"
-
 	stellarstation "github.com/infostellarinc/go-stellarstation/api/v1"
 	"github.com/infostellarinc/stellarcli/pkg/apiclient"
+	log "github.com/infostellarinc/stellarcli/pkg/logger"
 	"github.com/infostellarinc/stellarcli/util/printer"
 )
 
