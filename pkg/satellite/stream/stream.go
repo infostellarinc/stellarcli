0842f8666aff1bf555f2cb059985cbe1126d5f55
David Kuo <david.j.kuo@gmail.com>
2020-06-03 16:14:26 +0900

    write stats log from main event loop

--- pkg/satellite/stream/stream.go
+++ pkg/satellite/stream/stream.go
@@ -258,6 +258,7 @@ func (ss *satelliteStream) recvLoop() {
 				log.Debug("received data: streamId: %v, planId: %s, framing type: %s, size: %d bytes\n", ss.streamId, planId, telemetry.Framing, len(payload))
 				if ss.showStats {
 					metrics.collectTelemetry(telemetry)
+					metrics.tryLogStats()
 				}
 				if ss.correctOrder {
 					go func() {
@@ -295,6 +296,9 @@ func (ss *satelliteStream) recvLoop() {
 					}
 				}
 
+			}
+			if ss.showStats {
+				metrics.tryLogStats()
 			}
 		}
 	}
