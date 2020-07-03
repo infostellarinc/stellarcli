b1af4ac96e56378e870bde02a3100c62b0574c05
Daniel De Sousa <dan.desousa@gmail.com>
2020-07-03 17:21:23 +0900

    Explicitly cleanup.

--- pkg/satellite/stream/stream.go
+++ pkg/satellite/stream/stream.go
@@ -252,9 +252,11 @@ func (ss *satelliteStream) recvLoop() {
 					log.Printf("%s. Automatically retrying in %v", e, duration)
 				})
 			if rcErr != nil {
+				// This explicit cleanup is not the best solution and should be moved to a global
+				// (or higher-level) cleanup function.
+				ss.CloseFileWriter()
 				// Couldn't reconnect to the server, bailout.
-				log.Printf("error connecting to API stream: %v\n", err)
-				return
+				log.Fatalf("error connecting to API stream: %v\n", err)
 			}
 			log.Println("connected to the API stream.")
 		}
