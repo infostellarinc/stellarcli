44ac1f95dee09e793f53385404068be3671f2c1f
rockets3600 <38821665+rockets3600@users.noreply.github.com>
2021-07-05 14:48:08 +0900

    Removed timer based auto close code (#148)

--- cmd/flag/open_stream_flags.go
+++ cmd/flag/open_stream_flags.go
@@ -16,20 +16,15 @@
 package flag
 
 import (
-	"time"
-
 	"github.com/spf13/cobra"
 )
 
 var (
 	defaultEnableAutoClose = false
-	defaultAutoCloseDelay  = 5 * time.Second
 )
 
 type OpenStreamFlag struct {
 	EnableAutoClose bool
-	AutoCloseDelay  time.Duration
-	AutoCloseTime   string
 	StreamId        string
 }
 
@@ -37,11 +32,7 @@ type OpenStreamFlag struct {
 func (f *OpenStreamFlag) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&f.StreamId, "stream-id", "r", "", "The StreamId to resume.")
 	cmd.Flags().BoolVarP(&f.EnableAutoClose, "enable-auto-close", "", defaultEnableAutoClose,
-		"Deprecated: When set to true, the stream will close after a specified auto close time.")
-	cmd.Flags().DurationVarP(&f.AutoCloseDelay, "auto-close-delay", "", defaultAutoCloseDelay,
-		"Deprecated: The duration to wait before ending the stream with no more data incoming. Valid time units are \"s\", \"m\". Ex 1m30s. Range 1s to 10m")
-	cmd.Flags().StringVarP(&f.AutoCloseTime, "auto-close-time", "", "",
-		"Deprecated: The datetime (UTC) after which auto-closing will be enabled. Format 2006-01-02 15:04:05")
+		"When set to true, the stream will close after receiving the stream end message.")
 }
 
 // Validate flag values.
